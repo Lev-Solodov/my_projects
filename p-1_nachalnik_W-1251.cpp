07b88c8dfe09f7061efcb51c9ad88cdb33a1e762
Lev-Solodov <62211602+Lev-Solodov@users.noreply.github.com>
2022-01-25 17:52:27 +0300

    Create p-1_nachalnik_W-1251.cpp

---
+++ p-1_nachalnik_W-1251.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	cout << "***************** Программа 'Начальник (цикл)' *****************"<< endl;
+	string answer; // counter - schotchik
+	cout << "Начальник: 'выполнил ли ты задания, которые выдавал вчера?'"<< endl;
+	cin.getline(stream, string, separator)/*libo vmesto separator stavim '\n'*/
+	while (answer !="Да, конечно, сделал") /*Yes of course I did (in russian)*/
+	{
+		cout << "Через забор перепрыгнула овца " <<count<< "." << endl;
+		count +=1;
+	}
+	cout << "Счёт окончен - пора спать!"<< endl; /*The count is over - it's time to sleep!*/
+	system ("pause");
+return 0;
+}
